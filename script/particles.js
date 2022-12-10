2f6b64d6a470cf2ac28d84d0e2dd4e8043098742
jericodellomas <jericodellomas23@gmail.com>
2022-12-10 12:15:03 +0800

    fix position

--- script/particles.js
+++ script/particles.js
@@ -244,7 +244,7 @@ var pJS = function(tag_id, params){
       }
     }
 
-    /* position */
+    
     this.x = position ? position.x : Math.random() * pJS.canvas.w;
     this.y = position ? position.y : Math.random() * pJS.canvas.h;
 
