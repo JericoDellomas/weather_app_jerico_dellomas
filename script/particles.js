da9e69c5a1016d7cad1d8db07a86dcd63f6c4217
jericodellomas <jericodellomas23@gmail.com>
2022-12-10 12:20:47 +0800

    update

--- script/particles.js
+++ script/particles.js
@@ -435,7 +435,7 @@ var pJS = function(tag_id, params){
           p.y - radius / (2.66/3.5), 
           radius*2.66 / (pJS.particles.shape.polygon.nb_sides/3), 
           pJS.particles.shape.polygon.nb_sides, 
-          1 // sideCountDenominator
+          1 
         );
       break;
 
