69dacffd981fc4cdb642d5814c13075e300936fd
jericodellomas <jericodellomas23@gmail.com>
2022-12-10 12:08:31 +0800

    clearing

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function getWeather(latitude, longitude){
         });
 }
 
-// DISPLAY WEATHER TO UI
+
 function displayWeather(){
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
